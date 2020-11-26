fb74f28376a41894106bf7e347337f4e75d0f370
Maciej Sawicki <m.sawicki@samsung.com>
2020-11-26 14:12:23 +0100

    Presenter creation cleanup cleanup removed commented presenter creation options. Documentation covers example presenter creation.

--- FFmpegPlayer/EventLoop.cs
+++ FFmpegPlayer/EventLoop.cs
@@ -142,11 +142,11 @@ namespace FFmpegPlayer
                 .With(new SingleSourceDataProvider()
                     .Add(new BufferedGenericSource()
                         .AddHandler(OnError)
-                        //.Add("http://multiplatform-f.akamaihd.net/i/multi/april11/sintel/sintel-hd_,512x288_450_b,640x360_700_b,768x432_1000_b,1024x576_1400_m,.mp4.csmil/master.m3u8")
-                        .Add("rtsp://106.120.45.49/test.ts")
+                        .Add("http://multiplatform-f.akamaihd.net/i/multi/april11/sintel/sintel-hd_,512x288_450_b,640x360_700_b,768x432_1000_b,1024x576_1400_m,.mp4.csmil/master.m3u8")
                         .With(new DataSourceOptions()
-                            .Set(RtspOption.BufferSize, 1024 * 1024)
-                            .Set(RtspOption.Timeout, 5 * 1000000))));
+                            .Set(HttpOption.MultipleRequests, OptionValue.Yes)
+                            .Set(HttpOption.Reconnect, OptionValue.Yes)
+                            .Set(HttpOption.Seekable, OptionValue.Autodetect))));
 
             try
             {
